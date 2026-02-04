26a31041160ced2c43ed20b8117c5a9de37e7da5
Janet Xu <janet.m.xu@berkeley.edu>
2026-02-04 10:02:10 -0500

    added walmart projects

--- encrypt.sh
+++ encrypt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npx staticrypt fashion.html
-mv encrypted/fashion.html fashion-encrypted.html
+npx staticrypt fashion.html --template template.html --template-button Submit
+mv encrypted/fashion.html fashion-encrypted.html 
 rmdir encrypted
 echo "Encrypted!"
